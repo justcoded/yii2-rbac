0470fd8318ebfdde7990ef6c91bca865a6581469
oberest <oberest@justcoded.co>
2017-10-12 13:48:06 +0300

    patterm to delete comment

--- src/helpers/ScanHelper.php
+++ src/helpers/ScanHelper.php
@@ -65,8 +65,11 @@ class ScanHelper
 		$actions = [];
 		foreach ($controllers as $filename) {
 			$content = file_get_contents($filename);
-			$content = preg_replace('/(\/\*([^*]|[\r\n]|(\*([^/]|[\r\n])))*\*\/)|(\/\/.*)/i', '', $content);
-
+			$content = preg_replace(
+				"/(\/\*([^*]|[\r\n]|(\*+([^*\/]|[\r\n])))*\*\/)|(\/\/.*)/i",
+				'',
+				$content
+			);
 			// ignore abstract classes
 			if (false !== strpos($content, 'abstract class')) {
 				continue;
